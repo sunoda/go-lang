77ae706a851b7493d2981e98e77cca2273ea2ac2
sunoda <sky10323@gmail.com>
2020-04-14 23:42:20 +0800

    migrate

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	orm "api/database"
 	"api/router"
 	"os"
+	"api/migrate"
 )
 
 func main() {
 	defer orm.Eloquent.Close()
 	router := router.InitRouter()
+	migrate.UserTable()
 	router.Run(":"+os.Getenv("PORT"))
 }
